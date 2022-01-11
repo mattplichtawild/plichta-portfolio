3162cbba3d1d076a7a427b178daef194b3419df3
bigcatplichta <bigcatplichta@gmail.com>
2022-01-10 23:07:31 -0700

    rm/edit projectmodal

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
 import Zmage from "react-zmage";
 import Fade from "react-reveal";
+import ProjectModal from "./ProjectModal"
 
 let id = 0;
 class Portfolio extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const projects = this.props.data.projects.map(function (projects) {
-      let projectImage = "images/portfolio/" + projects.image;
-
+    const projects = this.props.data.projects.map( (project) => {
       return (
         <div key={id++} className="columns portfolio-item">
           <div className="item-wrap">
-            <Zmage alt={projects.title} src={projectImage} />
-            <div style={{ textAlign: "center" }}>{projects.title}</div>
+            <ProjectModal data={project} />
           </div>
         </div>
-      );
+      )
     });
 
     return (
