ba638ec2e82af09a294247f39fd782701c40c42b
bigcatplichta <bigcatplichta@gmail.com>
2022-01-11 23:02:26 -0700

    unused vars

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,13 +5,10 @@ class Contact extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const name = this.props.data.name;
     const email = this.props.data.email;
-    const street = this.props.data.address.street;
     const city = this.props.data.address.city;
     const state = this.props.data.address.state;
     const zip = this.props.data.address.zip;
-    const phone = this.props.data.phone;
     const message = this.props.data.contactmessage;
 
     return (
