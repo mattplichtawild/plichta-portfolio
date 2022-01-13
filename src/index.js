c72a1f4d7d116ded2c5363866255704fb7ac8623
bigcatplichta <bigcatplichta@gmail.com>
2022-01-12 19:40:51 -0700

    rm react-snap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { hydrate, render } from "react-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,12 +9,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// const rootElement = document.getElementById("root");
-// if (rootElement.hasChildNodes()) {
-//   hydrate(<App />, rootElement);
-// } else {
-//   render(<App />, rootElement);
-// }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
